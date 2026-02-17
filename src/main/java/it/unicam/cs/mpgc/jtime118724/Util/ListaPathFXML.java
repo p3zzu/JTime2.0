bd550918a2719e5e0286765d365f079ee7ab04e5
Pezzu-san <gianluca.perugini@studenti.unicam.it>
2026-02-17 15:44:23 +0100

    17/02/26 15:43

---
+++ src/main/java/it/unicam/cs/mpgc/jtime118724/Util/ListaPathFXML.java
@@ -0,0 +1,26 @@
+package it.unicam.cs.mpgc.jtime118724.Util;
+
+public enum ListaPathFXML {
+    LISTA_ATTIVITA("/SceneBuilderFiles/ListaAttivita.fxml"),
+    MODULO_CREAZIONE_ATTIVITA("/SceneBuilderFiles/ModuloCreazioneAttivita.fxml"),
+    START_MENU("/SceneBuilderFiles/StartMenu.fxml");
+
+    private final String fxml;
+    private ListaPathFXML(String s) {
+        this.fxml = s;
+    }
+
+    public String getFxml() {
+        return fxml;
+    }
+}
+
+/*
+VANTAGGI
+
+✅ Niente refusi (scrivere a mano le stringhe è pericoloso)
+
+✅ Se cambi cartella/nome file, lo cambi una sola volta nell’enum
+
+✅ Il codice diventa più leggibile: View.LISTA_ATTIVITA è più chiaro di una stringa
+ */

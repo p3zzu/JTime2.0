3570ddc4fc576a4f2c69bc7d5ad05f6790246889
Pezzu-san <gianluca.perugini@studenti.unicam.it>
2026-02-19 17:41:22 +0100

    19/02/26 16:55

--- src/main/java/it/unicam/cs/mpgc/jtime118724/Controller/Default/Attivita/ModuloModificaAttivitaController.java
+++ src/main/java/it/unicam/cs/mpgc/jtime118724/Controller/Default/Attivita/ModuloModificaAttivitaController.java
@@ -1,12 +1,17 @@
 package it.unicam.cs.mpgc.jtime118724.Controller.Default.Attivita;
 
+import it.unicam.cs.mpgc.jtime118724.Controller.Abstract.DataReceiver;
+import it.unicam.cs.mpgc.jtime118724.Controller.Abstract.IController;
+import it.unicam.cs.mpgc.jtime118724.Infrastructure.AppContext;
+import it.unicam.cs.mpgc.jtime118724.Model.Entities.Attivita;
+import it.unicam.cs.mpgc.jtime118724.Navigator.INavigator;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class ModuloModificaAttivitaController {
+public class ModuloModificaAttivitaController implements IController, DataReceiver <Attivita> {
     @FXML
     private TextField nome;
 
@@ -27,6 +32,41 @@ public class ModuloModificaAttivitaController {
 
     @FXML
     private Label statoAttivita;
+
+    private INavigator navigator;
+
+    private AppContext appContext;
+
+    private Attivita attivita; //appoggio
+
+
+
+
+   private void initialize(){
+
+   }
+
+
+
+
+
+    /**
+     * @param ctx
+     * @param nav
+     */
+    @Override
+    public void init(AppContext ctx, INavigator nav) {
+        this.appContext = ctx;
+        this.navigator = nav;
+    }
+
+    /**
+     * @param data
+     */
+    @Override
+    public void setData(Attivita data) {
+        this.attivita = data;
+    }
 
 
 }
